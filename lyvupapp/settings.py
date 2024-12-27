4afbb24e6723e7d5493b7bee90d167fb88c1eba0
amitwe2code <amitpatidar.we2code@gmail.com>
2024-12-27 17:13:06 +0530

    manage port deploy

--- lyvupapp/settings.py
+++ lyvupapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-fs#afy3k029)%8kif3w^jsbfzv!k8jdt@3ko&bqf^01sl5bbc=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','0.0.0.0','128.199.55.101']
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
@@ -161,7 +161,12 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
-    "http://127.0.0.1:5173",  
+    "http://127.0.0.1:5050", 
+    "http://128.199.55.101:",
+    "http://128.199.55.101:8000",
+    "http://128.199.55.101:5050",
+    
+
 ]
     
 
--- lyvupapp/views.py
+++ lyvupapp/views.py
@@ -107,7 +107,9 @@ class SignupView(APIView):
             print('data=',request.data)
             serializer = UserSerializer(data=request.data, context={'request': request})
             if serializer.is_valid():
-                print('request come')
+                # Password को hash करें
+                serializer.validated_data['password'] = make_password(serializer.validated_data['password'])
+                
                 profile_picture = request.FILES.get('profile_picture')
                 if profile_picture:
                     validation_result = self.validate_image(profile_picture)
@@ -118,7 +120,7 @@ class SignupView(APIView):
                             'message': validation_result['message'],
                             'data': None
                         }, status=status.HTTP_400_BAD_REQUEST)
-
+                
                 user = serializer.save()
                 print('success response')
                 return Response({
